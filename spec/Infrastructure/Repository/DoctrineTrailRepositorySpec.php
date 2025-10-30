e33706aadbe18a10f36db127db1f3c255452a408
euangt <euan.gillespie@outlook.com>
2025-10-30 14:46:14 +0000

    add in another spec

--- spec/Infrastructure/Repository/DoctrineTrailRepositorySpec.php
+++ spec/Infrastructure/Repository/DoctrineTrailRepositorySpec.php
@@ -25,4 +25,14 @@ class DoctrineTrailRepositorySpec extends ObjectBehavior
 
         $this->findOneById('12345')->shouldReturn($trail);
     }
+
+    function it_should_save_a_Trail(
+        EntityManagerInterface $entityManager,
+        Trail $trail
+    ) {
+        $entityManager->persist($trail)->shouldBeCalled();
+        $entityManager->flush()->shouldBeCalled();
+
+        $this->save($trail);
+    }
 }
