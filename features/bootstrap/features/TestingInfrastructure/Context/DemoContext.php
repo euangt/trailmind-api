c9bd3b691e57bd754cf441fd825c9f0fd2e15984
euangt <euan.gillespie@outlook.com>
2025-10-30 11:05:29 +0000

    adjust behat installation and adjust some config

---
+++ features/bootstrap/features/TestingInfrastructure/Context/DemoContext.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace features\TestingInfrastructure\Context;
+
+use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+
+/**
+ * Defines application features from the specific context.
+ */
+class DemoContext implements Context
+{
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+}
