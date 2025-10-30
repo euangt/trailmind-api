b47e8587ab92cb449dd4947c866eaa5846064f55
euangt <euan.gillespie@outlook.com>
2025-10-30 13:21:11 +0000

    delete data between feature calls

--- features/bootstrap/TestingInfrastructure/Context/Feature/FeatureContext.php
+++ features/bootstrap/TestingInfrastructure/Context/Feature/FeatureContext.php
@@ -14,6 +14,14 @@ use TestingInfrastructure\Context\AuthenticatedContext;
  */
 class FeatureContext implements Context
 {
+    // these tables are not edited during the test suite, so only need
+    // to be populated once
+    private const CORE_TABLES = [
+        'clients',
+        'roles',
+        'doctrine_migration_versions',
+    ];
+
     /**
      * @var ServiceProvider
      */
@@ -52,5 +60,38 @@ class FeatureContext implements Context
                 $this->version = $tag;
             }
         }
+    }
+
+    /**
+     * @BeforeScenario
+     */
+    public function resetDatabase()
+    {
+        $connection = self::$manager->getConnection();
+        $this->dropData($connection);
+    }
+
+    /**
+     * Remove any data that was created in the previous scenario so that we are
+     * working with a clear data set
+     */
+    private function dropData($connection)
+    {
+        $tables = array_filter($connection->createSchemaManager()->listTableNames(), function($table) {
+            return !in_array($table, self::CORE_TABLES);
+        });
+
+        //  required as product_categories has a self referencing foreign key
+        // $stmt = $connection->prepare("SET FOREIGN_KEY_CHECKS=0;");
+        // $stmt->executeQuery();
+
+        foreach ($tables as $table) {
+            $stmt = $connection->prepare("DELETE FROM " . $table);
+            $stmt->executeQuery();
+        }
+
+        // // make sure we put this back
+        // $stmt = $connection->prepare("SET FOREIGN_KEY_CHECKS=1;");
+        // $stmt->executeQuery();
     }
 }
