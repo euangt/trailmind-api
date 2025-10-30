65fb1249078f05a9f58f42a99310006c1d0ec1f0
euangt <euan.gillespie@outlook.com>
2025-10-30 12:14:39 +0000

    import that into the trail context

--- features/bootstrap/TestingInfrastructure/Context/Trail/TrailContext.php
+++ features/bootstrap/TestingInfrastructure/Context/Trail/TrailContext.php
@@ -2,6 +2,7 @@
 
 namespace TestingInfrastructure\Context\Trail;
 
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
@@ -11,6 +12,7 @@ use Behat\Step\Then;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Trailmind\Trail\Trail;
 use TestingInfrastructure\Services\ServiceProvider;
+use TestingInfrastructure\Context\Request\RequestContext;
 
 /**
  * Defines application features from the specific context.
@@ -18,11 +20,22 @@ use TestingInfrastructure\Services\ServiceProvider;
 class TrailContext implements Context
 {
     private ServiceProvider $services;
+    private RequestContext $requestContext;
 
     public function __construct(
         KernelInterface $kernel
     ) {
         $this->services = new ServiceProvider($kernel);
+    }
+
+    /**
+     * @BeforeScenario
+     *
+     * @param BeforeScenarioScope $scope
+     */
+    public function getOtherContexts(BeforeScenarioScope $scope)
+    {
+        $this->requestContext = $scope->getEnvironment()->getContext('TestingInfrastructure\Context\Request\RequestContext');
     }
 
     #[Given('there is the following trail:')]
