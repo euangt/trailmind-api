1ee1e38b22b10f6f321529baad20e51156ff09bd
euangt <euan.gillespie@outlook.com>
2025-10-30 13:20:40 +0000

    and call those from the builder

--- spec/Dto/Outbound/Trail/TrailBuilderSpec.php
+++ spec/Dto/Outbound/Trail/TrailBuilderSpec.php
@@ -4,11 +4,23 @@ namespace spec\Dto\Outbound\Trail;
 
 use Dto\Outbound\EntityBuilder;
 use Dto\Outbound\EntityDto;
+use Dto\Outbound\Trail\Decorators\DifficultyDecorator;
+use Dto\Outbound\Trail\Decorators\LengthDecorator;
+use Dto\Outbound\Trail\Decorators\TrailEntityDecorator;
+use Dto\Outbound\Trail\TrailBuilder;
 use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
 use Trailmind\Trail\Trail;
 
 class TrailBuilderSpec extends ObjectBehavior
 {
+    function let(
+        DifficultyDecorator $difficultyDecorator,
+        LengthDecorator $lengthDecorator
+    ) {
+        $this->beConstructedWith($difficultyDecorator, $lengthDecorator);
+    }
+
     function it_should_be_an_entity_builder()
     {
         $this->shouldBeAnInstanceOf(EntityBuilder::class);
@@ -16,8 +28,22 @@ class TrailBuilderSpec extends ObjectBehavior
 
     function it_should_initialise_a_trail(
         Trail $trail,
+        TrailEntityDecorator $trailEntityDecorator1,
+        TrailEntityDecorator $trailEntityDecorator2,
+        EntityDto $entityDto,
     ) {
         $this->setContext('context');
+        $prop = new \ReflectionProperty(TrailBuilder::class, 'decorators');
+        $prop->setAccessible(true);
+        $prop->setValue($this->getWrappedObject(), [
+            $trailEntityDecorator1->getWrappedObject(),
+            $trailEntityDecorator2->getWrappedObject()
+        ]);
+
+        $trailEntityDecorator1->withTrail($trail)->shouldBeCalled()->willReturn($trailEntityDecorator1);
+        $trailEntityDecorator1->decorate(Argument::type(EntityDto::class), 'context')->shouldBeCalled()->willReturn($entityDto);
+        $trailEntityDecorator2->withTrail($trail)->shouldBeCalled()->willReturn($trailEntityDecorator2);
+        $trailEntityDecorator2->decorate(Argument::type(EntityDto::class), 'context')->shouldBeCalled()->willReturn($entityDto);
 
         $trail->getId()->willReturn('1234');
         $trail->getName()->willReturn('Trail Name');
--- src/Dto/Outbound/Trail/TrailBuilder.php
+++ src/Dto/Outbound/Trail/TrailBuilder.php
@@ -4,6 +4,8 @@ namespace Dto\Outbound\Trail;
 
 use Dto\Outbound\EntityBuilder;
 use Dto\Outbound\Success;
+use Dto\Outbound\Trail\Decorators\DifficultyDecorator;
+use Dto\Outbound\Trail\Decorators\LengthDecorator;
 use Trailmind\Trail\Trail;
 
 class TrailBuilder extends EntityBuilder
@@ -16,6 +18,16 @@ class TrailBuilder extends EntityBuilder
      */
     private Trail $trail;
 
+    public function __construct(
+        DifficultyDecorator $difficultyDecorator,
+        LengthDecorator $lengthDecorator
+    ) {
+        $this->decorators = [
+            $difficultyDecorator,
+            $lengthDecorator
+        ];
+    }
+
     /**
      * @inheritdoc
      */
@@ -27,6 +39,19 @@ class TrailBuilder extends EntityBuilder
         $this->dto = new Success();
         $this->dto->add('id', $this->trail->getId());
         $this->dto->add('name', $this->trail->getName());
+
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function decorate(): self
+    {
+        foreach ($this->decorators as $decorator) {
+            $decorator->withTrail($this->trail)
+                    ->decorate($this->dto, $this->context);
+        }
 
         return $this;
     }
