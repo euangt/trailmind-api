61e2a99796894677acb8569034c7c6d0817eb1ea
euangt <euan.gillespie@outlook.com>
2025-10-31 11:21:50 +0000

    add user creation and password hashing to controller

--- spec/Controller/Register/RegisterControllerSpec.php
+++ spec/Controller/Register/RegisterControllerSpec.php
@@ -2,13 +2,37 @@
 
 namespace spec\Controller\Register;
 
+use Dto\Inbound\User\RegisteringUser;
 use Dto\Outbound\NoContent;
 use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Trailmind\User\User;
+use Trailmind\User\UserRepository;
 
 class RegisterControllerSpec extends ObjectBehavior
 {
-    function it_should_register_a_user()
-    {
-        $this->postRegisterAction()->shouldReturnAnInstanceOf(NoContent::class);
+    function let(
+        UserPasswordHasherInterface $passwordHasher,
+        UserRepository $userRepository
+    ) {
+        $this->beConstructedWith($passwordHasher, $userRepository);
+    }
+
+    function it_should_register_a_user(
+        UserPasswordHasherInterface $passwordHasher,
+        UserRepository $userRepository
+    ) {
+        $registeringUser = new RegisteringUser('name', 'email', 'password', 'username');
+        $passwordHasher->hashPassword(Argument::type(User::class), 'password')->willReturn('hashed-password');
+
+        $userRepository->save(Argument::that(function (User $user) {
+            return $user->getEmail() === 'email' &&
+                   $user->getName() === 'name' &&
+                   $user->getUsername() === 'username' &&
+                   $user->getPassword() === 'hashed-password';
+        }))->shouldBeCalled();
+
+        $this->postRegisterAction($registeringUser)->shouldReturnAnInstanceOf(NoContent::class);
     }
 }
--- src/Controller/Register/RegisterController.php
+++ src/Controller/Register/RegisterController.php
@@ -2,13 +2,37 @@
 
 namespace Controller\Register;
 
+use Dto\Inbound\User\RegisteringUser;
 use Dto\Outbound\NoContent;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Trailmind\User\User;
+use Trailmind\User\UserRepository;
 
 class RegisterController
 {
+    public function __construct(
+        private UserPasswordHasherInterface $passwordHasher,
+        private UserRepository $userRepository
+    ) {}
+
     #[Route('/v1.0/register', methods: ['POST'], name: 'api_v1.0_register')]
-    public function postRegisterAction(): NoContent {
+    public function postRegisterAction(
+        #[MapRequestPayload(acceptFormat: 'json')] RegisteringUser $registeringUser
+    ): NoContent {
+        $user = new User(
+            $registeringUser->email,
+            $registeringUser->name,
+            $registeringUser->username,
+            ['ROLE_USER']
+        );
+
+        $password = $this->passwordHasher->hashPassword($user, $registeringUser->password);
+        $user->setPassword($password);
+
+        $this->userRepository->save($user);
+
         return new NoContent();
     }
 }
