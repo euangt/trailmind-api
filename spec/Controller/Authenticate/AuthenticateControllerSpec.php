ca04f34a412bb35f197a587d1cc00deb4efc1fa4
euangt <euan.gillespie@outlook.com>
2025-11-04 14:42:45 +0000

    build that into the controller

--- spec/Controller/Authenticate/AuthenticateControllerSpec.php
+++ spec/Controller/Authenticate/AuthenticateControllerSpec.php
@@ -3,9 +3,14 @@
 namespace spec\Controller\Authenticate;
 
 use Dto\Inbound\User\AuthenticatingUser;
+use Dto\Internal\Authentication\AccessToken;
 use Dto\Outbound\Success;
+use Infrastructure\Oauth2Server\TokenManager;
 use PhpSpec\ObjectBehavior;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Trailmind\AuthenticationService\Exception\UnableToCreateAccessTokenException;
 use Trailmind\AuthenticationService\PasswordVerifier;
 use Trailmind\User\User;
 
@@ -13,31 +18,54 @@ class AuthenticateControllerSpec extends ObjectBehavior
 {
     function let(
         PasswordVerifier $passwordVerifier,
+        TokenManager $tokenManager
     ) {
         $this->beConstructedWith(
             $passwordVerifier,
+            $tokenManager
         );
     }
 
     function it_should_verify_a_user_with_password(
         User $user,
         PasswordVerifier $passwordVerifier,
+        TokenManager $tokenManager,
+        AccessToken $accessToken,
+        Request $request
     ) {
         $authenticatingUser = new AuthenticatingUser('email', 'password');
 
         $passwordVerifier->verifyPassword($user, 'password')->willReturn(true);
 
-        $this->postAuthenticateAction($authenticatingUser, $user)->shouldBeAnInstanceOf(Success::class);
+        $tokenManager->getAccessToken($request, $user)->willReturn($accessToken);
+
+        $this->postAuthenticateAction($authenticatingUser, $user, $request)->shouldBeAnInstanceOf(Success::class);
     }
 
     function it_should_throw_unauthorized_exception_for_invalid_password(
         User $user,
         PasswordVerifier $passwordVerifier,
+        Request $request
     ) {
         $authenticatingUser = new AuthenticatingUser('email', 'wrong-password');
 
         $passwordVerifier->verifyPassword($user, 'wrong-password')->willReturn(false);
 
-        $this->shouldThrow(UnauthorizedHttpException::class)->duringPostAuthenticateAction($authenticatingUser, $user);
+        $this->shouldThrow(UnauthorizedHttpException::class)->duringPostAuthenticateAction($authenticatingUser, $user, $request);
+    }
+
+    function it_should_throw_bad_request_exception_when_unable_to_create_access_token(
+        User $user,
+        PasswordVerifier $passwordVerifier,
+        TokenManager $tokenManager,
+        Request $request
+    ) {
+        $authenticatingUser = new AuthenticatingUser('email', 'password');
+
+        $passwordVerifier->verifyPassword($user, 'password')->willReturn(true);
+
+        $tokenManager->getAccessToken($request, $user)->willThrow(UnableToCreateAccessTokenException::class);
+
+        $this->shouldThrow(BadRequestHttpException::class)->duringPostAuthenticateAction($authenticatingUser, $user, $request);
     }
 }
--- src/Controller/Authenticate/AuthenticateController.php
+++ src/Controller/Authenticate/AuthenticateController.php
@@ -5,9 +5,13 @@ namespace Controller\Authenticate;
 use Application\ValueResolver\CustomisableValueResolver;
 use Dto\Inbound\User\AuthenticatingUser;
 use Dto\Outbound\Success;
+use Infrastructure\Oauth2Server\TokenManager;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Trailmind\AuthenticationService\Exception\UnableToCreateAccessTokenException;
 use Trailmind\AuthenticationService\PasswordVerifier;
 use Trailmind\User\User;
 
@@ -15,15 +19,23 @@ class AuthenticateController
 {
     public function __construct(
         private PasswordVerifier $passwordVerifier,
+        private TokenManager $tokenManager,
     ) {}
 
     #[Route('/v1.0/authenticate', methods: ['POST'], name: 'api_v1.0_authenticate')]
     public function postAuthenticateAction(
         #[MapRequestPayload(acceptFormat: 'json')] AuthenticatingUser $authenticatingUser,
         #[CustomisableValueResolver('entity', false, ['class' => User::class, 'mapping' => ['email' => 'email']])] $user,
+        Request $request,
     ): Success {
         if (!$this->passwordVerifier->verifyPassword($user, $authenticatingUser->password)) {
             throw new UnauthorizedHttpException('Invalid credentials');
+        }
+
+        try {
+            $accessToken = $this->tokenManager->getAccessToken($request, $user);
+        } catch (UnableToCreateAccessTokenException $utcate) {
+            throw new BadRequestHttpException('Unable to create access token');
         }
 
         return new Success();
