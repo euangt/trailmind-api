e678f27e979c2dde18eeefeda7480c6cc1458c59
euangt <euan.gillespie@outlook.com>
2026-01-05 15:26:29 +0000

    and setup the controller

---
+++ spec/Controller/Trail/TrailsControllerSpec.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace spec\Controller\Trail;
+
+use Dto\Outbound\Success;
+use Dto\Outbound\Trail\TrailCollectionBuilder;
+use PhpSpec\ObjectBehavior;
+use Trailmind\Trail\Trail;
+use Trailmind\Trail\TrailRepository;
+
+class TrailsControllerSpec extends ObjectBehavior
+{
+    function let(
+        TrailRepository $trailRepository,
+        TrailCollectionBuilder $trailCollectionBuilder
+    ) {
+        $this->beConstructedWith(
+            $trailRepository,
+            $trailCollectionBuilder
+        );
+    }
+
+    function it_should_return_a_trail(
+        TrailRepository $trailRepository,
+        TrailCollectionBuilder $trailCollectionBuilder,
+        Trail $trail1,
+        Trail $trail2,
+        Trail $trail3,
+        Success $success
+    ) {
+        $trailRepository->findAll()->willReturn([$trail1, $trail2, $trail3]);
+
+        $trailCollectionBuilder->setContext('v1.0_view_trails')->shouldBeCalled()->willReturn($trailCollectionBuilder);
+        $trailCollectionBuilder->build([$trail1, $trail2, $trail3])->willReturn($success);
+
+        $this->getTrailAction()->shouldReturn($success);
+    }
+}
---
+++ src/Controller/Trail/TrailsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Controller\Trail;
+
+use Dto\Outbound\Success;
+use Dto\Outbound\Trail\TrailCollectionBuilder;
+use Symfony\Component\Routing\Annotation\Route;
+use Trailmind\Trail\TrailRepository;
+
+class TrailsController
+{
+    public function __construct(
+        private readonly TrailRepository $trailRepository,
+        private readonly TrailCollectionBuilder $trailCollectionBuilder,
+    ) {}
+
+    #[Route('/v1.0/trails', methods: ['GET'], name: 'api_v1.0_view_trails')]
+    public function getTrailAction(): Success {
+        return $this->trailCollectionBuilder
+            ->setContext('v1.0_view_trails')
+            ->build($this->trailRepository->findAll());
+    }
+}
